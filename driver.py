110c09749ab9181f12e61e860b243dd4832e547e
U1and0 <e01.ando60@gmail.com>
2016-10-26 19:05:37 +0900

    add drriver.py data driver

---
+++ driver.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+
+
+def merge(ser_list: list, nameargs: list) -> pd.core.frame.DataFrame:
+    """
+    一つのデータフレームにまとめる
+    引数:
+        ser_list: シリーズが入ったリスト(list of pd.Series type)
+        nameargs: 作成されるdfのcolumn名が入ったリスト(list of string type)
+    戻り値:
+        df: シリーズがマージされたデータフレーム(pd.DataFrame type)
+    """
+    cc = pd.DataFrame(ser_list)
+    df = cc.T
+    df.columns = nameargs
+    return df
+
+
+def chu(df_in: pd.core.frame.DataFrame,
+        columns_name: str,
+        low: float,
+        high: float) -> pd.core.frame.DataFrame:
+    """
+    df_inのcolumns_nameの列からlow以上high以下の
+    値を示す部分を抽出する
+    引数:
+        df_in: (pd.DataFrame type)
+    戻り値:
+        df_out: 値が抽出されたデータフレーム(pd.DataFrame type)
+    """
+    a = df_in[df_in[columns_name] >= low]
+    df_out = a[a[columns_name] <= high]
+    return df_out
+
+
+if __name__ == '__main__':
+    ddd = pd.DataFrame(np.arange(10), columns=['go'])
+    chu(ddd, 'go', 2, 4)  # 234のデータフレームが返る
+    # ccc=pd.Series(np.arange(10), name='go')
+    # chu(ccc, 'go', 2, 4)  # エラーが返る
+    # aaa=np.arange(10)
+    # chu(aaa, 'go', 2, 4)  # エラーが返る
