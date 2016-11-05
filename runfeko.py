93ef376f1b0e3779d2d7687d1dbe58a9fafb6d81
U1and0 <e01.ando60@gmail.com>
2016-11-05 23:32:10 +0900

    encapsulation

--- runfeko.py
+++ runfeko.py
@@ -26,21 +26,65 @@ import subprocess as sp
 from datetime import datetime
 from tqdm import tqdm
 
-files = glob.glob('../*.dat')
-default_command = ['echo', 'foo']
-count = 0
+
+def confirm(files:str) -> str:
+    """
+    実行ファイルの確認
+    正しい値が入力されるまで繰り返し。
+
+    引数:
+        files: globで探されたファイル名(str型)
+    戻り値:
+        inp:y -> True / n -> False(bool型)
+    """
+    for file in files:
+        print(file)
+    dic = {'y': True, 'yes': True, 'n': False, 'no': False}
+    while True:
+        try:
+            inp = dic[input('以上のファイルを実行します。よろしいですか？ y/n? >>>').lower()]
+            break
+        except:
+            pass
+        print('Error! Input again.')
+    return inp
 
 
-for file in tqdm(files):
-    count += 1
-    print('実行サイクル: %d/%d' % (count, len(files)))
-    ts = datetime.today()
-    command = []
-    command = default_command.copy()
-    command.insert(1, file)
-    print('実行コマンド: ', *command)
-    sp.call(command)
-    te = datetime.today()
-    print('開始時刻: ', ts)
-    print('終了時刻: ', te)
-    print('実行時間: ', te - ts)
+def excute(default_command:list, *files:str):
+    """
+    runfekoの実行
+
+    引数: 
+        dafault_command:(リスト型)
+        dafault_files:ファイル名(str型)
+    戻り値:なし
+    """
+    count = 0
+    if confirm(files):
+        print('--\nyesが入力されました。処理を続行します。\n')
+        for file in tqdm(files):
+            count += 1
+            print('実行サイクル: %d/%d' % (count, len(files)))
+
+            ts = datetime.today()
+
+            command = []  # 初期化
+            command = default_command.copy()
+            command.insert(1, file)
+            print('実行コマンド: ', *command)
+            sp.call(command)
+
+            te = datetime.today()
+
+            print('開始時刻: ', ts)
+            print('終了時刻: ', te)
+            print('実行時間: ', te - ts)
+    else:
+        print('--\nNoが入力されました。\n処理終了します。')
+
+
+if __name__ == '__main__':
+    # files = glob.glob('../*.dat')  # TESTcommand
+    files = glob.glob(input('正規表現でファイル名を入力してください。>>> '))
+    command = ['echo', 'foo']
+    excute(command, *files)
